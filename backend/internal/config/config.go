d79eb9ee39ba7339d7a4f14a8a58ad9543d8174f
Dnreikronos <dnreikronos2@gmail.com>
2025-04-02 18:59:32 -0300

    feature: create db config struct

--- backend/internal/config/config.go
+++ backend/internal/config/config.go
@@ -7,3 +7,11 @@ type config struct {
 type APIConfig struct {
 	Port string
 }
+type DBConfig struct {
+	Host     string
+	Port     string
+	User     string
+	Password string
+	Database string
+	TimeZone string
+}
