9898b29c0203fc6a6ffd17c6ddeb2420f3f06778
Dnreikronos <dnreikronos2@gmail.com>
2025-04-02 20:16:09 -0300

    feaute: create UserResponse struct

--- backend/internal/models/user.go
+++ backend/internal/models/user.go
@@ -7,3 +7,11 @@ type SignInInput struct {
 	Email    string `json:"email" binding:"required"`
 	Password string `json:"password" binding:"required"`
 }
+type UserResponse struct {
+	ID        uuid.UUID `json:"id,omitempty"`
+	Name      string    `json:"name,omitempty"`
+	Email     string    `json:"email,omitempty"`
+	UpdatedAt time.Time `json:"updated_at"`
+	CreatedAt time.Time `json:"created_at"`
+	Verified  bool      `json:"verified"`
+}
