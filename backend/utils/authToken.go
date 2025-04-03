29393ae890d05c245de0faec9eabd14b30c173ee
Dnreikronos <dnreikronos2@gmail.com>
2025-04-03 19:28:14 -0300

    feature: create function that validate the JWT token

--- backend/utils/authToken.go
+++ backend/utils/authToken.go
@@ -1,3 +1,13 @@
+package utils
+
+import (
+	"encoding/base64"
+	"fmt"
+	"time"
+
+	"github.com/golang-jwt/jwt"
+)
+
 func CreateToken(ttl time.Duration, payload interface{}, privateKey string) (string, error) {
 	decodedPrivateKey, err := base64.StdEncoding.DecodeString(privateKey)
 	if err != nil {
@@ -23,4 +33,32 @@ func CreateToken(ttl time.Duration, payload interface{}, privateKey string) (str
 	}
 
 	return token, nil
+}
+
+func ValidateToken(token string, publicKey string) (interface{}, error) {
+	decodedPublicKey, err := base64.StdEncoding.DecodeString(publicKey)
+	if err != nil {
+		return nil, fmt.Errorf("could not decode: %w", err)
+	}
+
+	key, err := jwt.ParseRSAPrivateKeyFromPEM(decodedPublicKey)
+	if err != nil {
+		return "", fmt.Errorf("Validate: parse key: %w", err)
+	}
+
+	parsedToken, err := jwt.Parse(token, func(t *jwt.Token) (interface{}, error) {
+		if _, ok := t.Method.(*jwt.SigningMethodRSA); !ok {
+			return nil, fmt.Errorf("unexpected method: %s", t.Header["alg"])
+		}
+		return key, nil
+	})
+	if err != nil {
+		return nil, fmt.Errorf("validate: %w", err)
+	}
+
+	claims, ok := parsedToken.Claims.(jwt.MapClaims)
+	if !ok || !parsedToken.Valid {
+		return nil, fmt.Errorf("Validate: invalid token")
+	}
+	return claims["sub"], nil
 }
