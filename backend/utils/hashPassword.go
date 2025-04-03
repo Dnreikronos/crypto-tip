64e00a459b88162a9716b4b0526ae3d33786523d
Dnreikronos <dnreikronos2@gmail.com>
2025-04-03 19:28:51 -0300

    feature: create function for compare the user password that have in the database with the canidatePassword

--- backend/utils/hashPassword.go
+++ backend/utils/hashPassword.go
@@ -1,4 +1,15 @@
+package utils
+
+import "golang.org/x/crypto/bcrypt"
+
+
+
 func HashPassowrd(password string) string {
 	hashedPassword, _ := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
 	return string(hashedPassword)
+}
+
+func ComparePassword(hashedPassword string, candidatePassword string) error {
+	return bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(candidatePassword))
+
 }
