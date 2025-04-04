2534d3d1e9e3295cb8b28e3b120c9ce71f1eefa6
Dnreikronos <dnreikronos2@gmail.com>
2025-04-03 21:17:02 -0300

    refacotr: adjust signing method to rs256

--- backend/utils/authToken.go
+++ backend/utils/authToken.go
@@ -27,13 +27,7 @@ func CreateToken(ttl time.Duration, payload interface{}, privateKey string) (str
 		"nbf": now.Unix(),
 	}
 
-	claims := make(jwt.MapClaims)
-	claims["sub"] = payload
-	claims["exp"] = now.Add(ttl).Unix()
-	claims["iat"] = now.Unix()
-	claims["nbf"] = now.Unix()
-
-	token, err := jwt.NewWithClaims(jwt.SigningMethodES256, claims).SignedString(key)
+	token, err := jwt.NewWithClaims(jwt.SigningMethodRS256, claims).SignedString(key)
 	if err != nil {
 		return "", fmt.Errorf("create: sign token: %w", err)
 	}
