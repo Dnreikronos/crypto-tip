714092b63482ca7716024b1d7ef499db1192ba75
Dnreikronos <dnreikronos2@gmail.com>
2025-04-03 21:15:59 -0300

    refactor: adjust error handlers

--- backend/utils/authToken.go
+++ backend/utils/authToken.go
@@ -11,12 +11,12 @@ import (
 func CreateToken(ttl time.Duration, payload interface{}, privateKey string) (string, error) {
 	decodedPrivateKey, err := base64.StdEncoding.DecodeString(privateKey)
 	if err != nil {
-		return "", fmt.Errorf("Could not decode key: %w", err)
+		return "", fmt.Errorf("could not decode key: %w", err)
 	}
 
 	key, err := jwt.ParseRSAPrivateKeyFromPEM(decodedPrivateKey)
 	if err != nil {
-		return "", fmt.Errorf("Create: parse key: %w", err)
+		return "", fmt.Errorf("create: parse key: %w", err)
 	}
 
 	now := time.Now().UTC()
