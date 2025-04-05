6bebe1072ac2324d4af37ee0bb48024a2d2a7139
Dnreikronos <dnreikronos2@gmail.com>
2025-04-05 17:31:45 -0300

    feature: create function for create user handler

--- backend/internal/handlers/user.go
+++ backend/internal/handlers/user.go
@@ -16,3 +16,45 @@ func GenereateToken(user models.User) (string, error) {
 	token := jwt.NewWithClaims(jwt.SigningMethodES256, claims)
 	return token.SignedString(jwtSecret)
 }
+func CreateUserHandler(c *gin.Context) {
+	var input models.SignInInput
+	if err := c.ShouldBindJSON(&input); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	hashedPassword, err := HashPassword(input.Password)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to hash password"})
+		return
+	}
+	newUser := models.User{
+		Email:    input.Email,
+		Password: hashedPassword,
+		Verified: true,
+	}
+	db := c.MustGet("db").(*gorm.DB)
+	if err := db.Create(&newUser).Error; err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to create user"})
+	}
+	c.JSON(http.StatusCreated, models.FilteredResponse(newUser))
+}
+
+func LoginHandler(c *gin.Context) {
+	var input models.SignInInput
+	if err := c.ShouldBindJSON(&input); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	var existingUser models.User
+	db := c.MustGet("db").(*gorm.DB)
+	if err := db.Where("email = ?", input.Email).First(&existingUser).Error; err != nil {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid Credentials"})
+		return
+	}
+	token, err := GenereateToken(existingUser)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Could not generate token"})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"token": token})
+}
