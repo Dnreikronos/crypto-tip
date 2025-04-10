d990786829703157646eda4871b7159278bd603e
Dnreikronos <dnreikronos2@gmail.com>
2025-04-10 19:41:22 -0300

    feature: create delete project handler

--- backend/internal/handlers/project.go
+++ backend/internal/handlers/project.go
@@ -1,3 +1,14 @@
+package handlers
+
+import (
+	"net/http"
+
+	"github.com/Dnreikronos/crypto-tip/internal/models"
+	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
+
 func CreateProjectHandler(c *gin.Context) {
 	var input models.ProjectInput
 	if err := c.ShouldBindJSON(&input); err != nil {
@@ -26,4 +37,27 @@ func CreateProjectHandler(c *gin.Context) {
 	}
 
 	c.JSON(http.StatusCreated, models.ProjectToResponse(newProject, false))
+}
+
+func DeleteProjectHandler(c *gin.Context) {
+	userID, exists := c.Get("UserID")
+	if !exists {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "User not authenticated"})
+		return
+	}
+
+	id := c.Param("id")
+	db := c.MustGet("db").(gorm.DB)
+
+	var project models.Project
+	if err := db.First(&project, "id = ? AND user_id = ?", id, userID).Error; err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"error": "Project not found or not owned by any user"})
+		return
+	}
+
+	if err := db.Delete(&project).Error; err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to delete project"})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"message": "Project deleted sucessfulyy"})
 }
