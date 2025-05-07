af69cf4528eb00356cbc554d7a65478868312dbc
Dnreikronos <dnreikronos2@gmail.com>
2025-05-06 22:14:39 -0300

    feature: create ProjectsContent client side

---
+++ frontend/src/app/my-projects/ProjectsContent.tsx
@@ -0,0 +1,26 @@
+'use client';
+
+import { useSuspenseQuery } from '@tanstack/react-query';
+import { ProjectsTable } from '@/components/ui/ProjectsTable';
+import { EmptyState } from '@/components/ui/EmptyState';
+import TableSkeleton from '@/components/ui/TableSkeleton';
+import { getProjects } from './getProjects';
+import { Suspense } from 'react';
+
+export default function ProjectsContent() {
+	return (
+		<Suspense fallback={<TableSkeleton />}>
+			<Projects />
+		</Suspense>
+	);
+}
+
+function Projects() {
+	const { data: projects } = useSuspenseQuery({
+		queryKey: ['projects'],
+		queryFn: getProjects,
+	});
+
+	if (projects.length === 0) return <EmptyState />;
+	return <ProjectsTable initialProjects={projects} />;
+}
