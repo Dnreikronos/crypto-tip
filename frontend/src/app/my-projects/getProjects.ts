289374142cd1ceaef8dce44695d79e5101c13034
Dnreikronos <dnreikronos2@gmail.com>
2025-05-07 20:04:06 -0300

    feature: create getProjects use client

---
+++ frontend/src/app/my-projects/getProjects.ts
@@ -0,0 +1,36 @@
+'use client';
+
+import { z } from 'zod';
+import Cookies from 'js-cookie';
+
+const ProjectSchema = z.object({
+	id: z.string(),
+	title: z.string(),
+	description: z.string(),
+	goal: z.number(),
+	raised: z.number(),
+	created_at: z.coerce.date(),
+});
+
+const ProjectsSchema = z.array(ProjectSchema);
+
+export async function getProjects() {
+	if (typeof window === 'undefined') {
+		return [];
+	}
+
+	const token = Cookies.get('token');
+	if (!token) return [];
+
+	const res = await fetch('http://localhost:9090/user/projects', {
+		headers: {
+			Authorization: `Bearer ${token}`,
+		},
+		credentials: 'include',
+		cache: 'no-store',
+	});
+
+	if (!res.ok) throw new Error('Failed to fetch projects');
+	const data = await res.json();
+	return ProjectsSchema.parse(data);
+}
