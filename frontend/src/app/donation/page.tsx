de6e7d48b0761057ec2ed5a016b7d160156d861e
Dnreikronos <dnreikronos2@gmail.com>
2025-05-11 22:10:58 -0300

    refactor: remove unecessary try catch

--- frontend/src/app/donation/page.tsx
+++ frontend/src/app/donation/page.tsx
@@ -8,15 +8,8 @@ interface DonationPageProps {
 
 export default async function DonationPage({ searchParams }: DonationPageProps) {
 	const projectId = searchParams.projectId as string;
-	let project = null;
-
-	if (projectId) {
-		try {
-			project = await getProject(projectId);
-		} catch (error) {
-			// Project will remain null if there's an error
-		}
-	}
+	const project = projectId ? await getProject(projectId) : null;
 
 	return <DonationPageClient project={project} />;
 }
+
