6e3de77da47d9dee3cea135fedbc4ce2e1b6c618
Dnreikronos <dnreikronos2@gmail.com>
2025-06-19 19:28:56 -0300

    refactor: change database connection for get directly the postgres source

--- backend/internal/storage/connection/connection.go
+++ backend/internal/storage/connection/connection.go
@@ -9,23 +9,26 @@ import (
 	"gorm.io/gorm"
 )
 
-
 func OpenConnection() (*gorm.DB, error) {
-	connectionInfo := fmt.Sprintf(
-		"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable TimeZone=%s",
-		os.Getenv("DB_HOST"),
-		os.Getenv("DB_PORT"),
-		os.Getenv("DB_USER"),
-		os.Getenv("POSTGRES_PASSWORD"),
-		os.Getenv("DB_NAME"),
-		os.Getenv("POSTGRES_TIME_ZONE"),
-	)
+	dsn := os.Getenv("POSTGRES_SOURCE")
+	if dsn == "" {
+		dsn = fmt.Sprintf(
+			"host=%s port=%s user=%s password=%s dbname=%s sslmode=require TimeZone=%s",
+			os.Getenv("DB_HOST"),
+			os.Getenv("DB_PORT"),
+			os.Getenv("DB_USER"),
+			os.Getenv("POSTGRES_PASSWORD"),
+			os.Getenv("DB_NAME"),
+			os.Getenv("POSTGRES_TIME_ZONE"),
+		)
+	}
 
-	db, err := gorm.Open(postgres.Open(connectionInfo), &gorm.Config{})
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		return nil, err
 	}
 
-	log.Println("Sucessfully connected to the database")
+	log.Println("Successfully connected to the database")
 	return db, nil
 }
+
