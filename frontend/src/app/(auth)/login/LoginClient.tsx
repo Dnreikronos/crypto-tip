a6c615391b509ad2597bcd90ddbbe629ce86bad5
yuribodo <yemotalara@gmail.com>
2025-06-21 19:18:41 -0300

    refactor: comment out unused password recovery routes in config and login component for improved clarity

--- frontend/src/app/(auth)/login/LoginClient.tsx
+++ frontend/src/app/(auth)/login/LoginClient.tsx
@@ -157,12 +157,12 @@ export default function LoginClient() {
                     >
                       Password
                     </Label>
-                    <Link
+                    {/* <Link
                       href="/forgot-password"
                       className="text-xs text-cyan-400 hover:underline"
                     >
                       Forgot password?
-                    </Link>
+                    </Link> */}
                   </div>
                   <div className="relative">
                     <Lock className="absolute left-3 top-3 h-4 w-4 text-gray-400" />
