7da580602cb400d91cd0c490564793836ac18cc8
Dnreikronos <dnreikronos2@gmail.com>
2025-06-21 13:16:22 -0300

    refactor[deployScript]: change again the wallet addr

--- crypto/script/Deploy.s.sol
+++ crypto/script/Deploy.s.sol
@@ -13,7 +13,7 @@ contract DeployDonationContract is Script {
         vm.startBroadcast(deployerPrivateKey);
 
         // Deploy the contract with your Sepolia address as the fee wallet
-        address feeWallet = 0xFDBb4e98aE5078Ad5AeE25a36401A2a27E03530b;
+        address feeWallet = 0x9F18650f3d73beAFB0d4A3356B9888A986871169;
         DonationContract donationContract = new DonationContract(feeWallet);
 
         vm.stopBroadcast();
