0d9481e521df623847dbb1df22c50561f1d6fcef
Dnreikronos <dnreikronos2@gmail.com>
2025-06-23 19:47:20 -0300

    refactor: adjust identation

--- frontend/src/config/wagmi.ts
+++ frontend/src/config/wagmi.ts
@@ -1,13 +1,11 @@
 import { http, createConfig } from "@wagmi/core";
-import { mainnet, linea, lineaSepolia } from "wagmi/chains";
+import { mainnet } from "wagmi/chains";
 import { metaMask } from "wagmi/connectors";
 
 export const config = createConfig({
-  chains: [mainnet, linea, lineaSepolia],
+  chains: [mainnet],
   connectors: [metaMask()],
   transports: {
     [mainnet.id]: http(),
-    [linea.id]: http(),
-    [lineaSepolia.id]: http(),
   },
 });
